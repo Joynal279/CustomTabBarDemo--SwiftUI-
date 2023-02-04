69e259c853a8ca328e5e02219edc2819a33e09a0
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-02-05 01:08:24 +0600

    Initial Commit

---
+++ CustomTabBarDemo/CustomTabBarDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarDemoApp.swift
+//  CustomTabBarDemo
+//
+//  Created by Joynal Abedin on 5/2/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
